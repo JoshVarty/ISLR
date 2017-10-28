7c0adceb0c70028e131150ec3aeca2bbe8b4fcad
Josh Varty <joshvarty@gmail.com>
2017-10-28 12:58:10 -0400

    Scatter Matrix of first ten elements

--- ISLR/Chapter2.py
+++ ISLR/Chapter2.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 
 path = "../input/College.csv"
 df = pd.read_csv(path)
 
 #Set rownames equal to the first column (School name)
 df = df.set_index(df.columns[0])
+
+to_plot = df.iloc[:,0:10]
+scatter_matrix(to_plot, alpha=0.2, figsize=(10,10), diagonal="kpe")
+plt.show()
